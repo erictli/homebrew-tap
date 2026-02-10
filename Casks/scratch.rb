7d49986101e1dbbd6f4398b52fd908f14fc47795
erictli <hi@ericli.io>
2026-02-10 08:09:45 -0500

    Updating for v0.3.0

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -1,6 +1,6 @@
 cask "scratch" do
-  version "0.2.0"
-  sha256 "79107641adf332eefe29d655a4c99b463b80e1783114cf4316d68d9c389e3538"
+  version "0.3.0"
+  sha256 "4b9821ef9d3755e7620112f307c21c63cc4173e6344ff8fb6939669f3c094c1c"
 
   url "https://github.com/erictli/scratch/releases/download/v#{version}-beta/Scratch_#{version}_aarch64.dmg"
   name "Scratch"
@@ -11,12 +11,6 @@ cask "scratch" do
   depends_on arch: :arm64
 
   app "Scratch.app"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/Scratch.app"],
-                   sudo: false
-  end
 
   zap trash: [
     "~/Library/Application Support/com.scratch.app",
