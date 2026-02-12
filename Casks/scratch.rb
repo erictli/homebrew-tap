00fd6608fec5ada5c700aebbe0dc4ed8a6aef96b
erictli <hi@ericli.io>
2026-02-11 19:31:44 -0500

    Updating for v0.4.0

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -1,14 +1,13 @@
 cask "scratch" do
-  version "0.3.0"
-  sha256 "c9e40cf043c796fab7e9025306449024dc7826dd389b3331f4bccdbf157a1c2a"
+  version "0.4.0"
+  sha256 "880ad74328e88299b04bd9b4d5023951829c295cb56095327f099354c1a7a4d2"
 
-  url "https://github.com/erictli/scratch/releases/download/v#{version}-beta/Scratch_#{version}_aarch64.dmg"
+  url "https://github.com/erictli/scratch/releases/download/v#{version}/Scratch_#{version}_universal.dmg"
   name "Scratch"
   desc "Minimalist, offline-first markdown note-taking app"
   homepage "https://github.com/erictli/scratch"
 
-  depends_on macos: ">= :monterey"
-  depends_on arch: :arm64
+  depends_on macos: ">= :catalina"
 
   app "Scratch.app"
 
