ba3f7963ddb41a4df96090409679b692c9a37474
erictli <hi@ericli.io>
2026-03-04 13:53:02 -0500

    Updating for v0.8.0

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -1,6 +1,6 @@
 cask "scratch" do
-  version "0.7.1"
-  sha256 "c6c8d2aace7a6ae77d4b12427c1fa8df118196d6d34da49af3b81ab090be6e5e"
+  version "0.8.0"
+  sha256 "bacf4bb77a96ab12c36feca3ba6b6d416e92956545185f0a914425258b82099a"
 
   url "https://github.com/erictli/scratch/releases/download/v#{version}/Scratch_#{version}_universal.dmg"
   name "Scratch"
